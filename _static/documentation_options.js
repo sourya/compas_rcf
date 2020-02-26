cea9c8cf4a0614a246761fb90c75ed3b96e63598
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-26 22:53:37 +0000

    Deploy tetov/compas_rcf to github.com/tetov/compas_rcf.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.21',
+    VERSION: '0.1.22.dev2+g2f75a87',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
